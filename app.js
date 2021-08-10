c5d595276a4c7d2de7474c0e9ba2d299f5ecf740
Nate Lee <nleepercussion@gmail.com>
2021-08-10 15:15:49 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3333;
+
+const cors = require('cors');
+
+const express = require('express');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+    console.log(`Server is running at ${hostname}:${port}`)
+});
+
+const rootController = require('./routes/index');
+
+app.use('/', rootController);
+
