03007458b939cb02419c7c78746f7d8d56104027
Nate Lee <nleepercussion@gmail.com>
2021-08-10 17:37:48 -0400

    Created route files, finished users, items, orders routes

---
+++ routes/orders.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express');
+const OrdersModel = require('..models/Orders');
+const router = express.Router();
+
+//GET by userID or get all
+
+router.get('/', async (req, res) => {
+    if (!!req.body.user_id) {
+        const filteredData = await OrdersModel.getByUserID(req.body.user_id);
+        if (filteredData.count = 0) {
+            res.send(filteredData)
+        }
+        res.json(filteredData).status(200);
+    } else {
+        const allData = await OrdersModel.getAll();
+        res.json(allData).status(200);
+    }
+});
+
+module.exports = router;
