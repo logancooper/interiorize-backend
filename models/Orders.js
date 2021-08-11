fc41c0f5e698a609c2d2c5f38c21ccba57e52313
Nate Lee <nleepercussion@gmail.com>
2021-08-11 17:03:08 -0400

    Deleted unused route. Added Items, Orders, Users models

---
+++ models/Orders.js
@@ -0,0 +1,40 @@
+const { use } = require('../routes');
+const db = require('./conn');
+
+class OrdersModel {
+    constructor(id, user_id, created_date) {
+        this.id = id;
+        this.user_id = user_id;
+        this.created_date = created_date;
+    };
+
+    //Get all orders in the database
+    static async getAll() {
+        try {
+            const response = await db.any(`
+                SELECT * FROM orders; `
+            )
+            return response;
+        } catch (error) {
+            console.error('ERROR', error)
+            return error
+        }
+    }
+
+    // Get all orders for a user and sort by newest
+    static async getByUserID(user_id) {
+        try {
+            const response = await db.any(`
+                SELECT * FROM orders
+                WHERE user_id = ${user_id}
+                ORDER BY created_date DESC; `
+            )
+            return response;
+        } catch (error) {
+            console.error('ERROR', error)
+            return error
+        }
+    }
+}
+
+module.exports = OrdersModel;
