a47c43c08997f815856d03fc44fb4de5ac3d9aa1
Nate Lee <nleepercussion@gmail.com>
2021-08-11 15:48:20 -0400

    Added public images folder with 56 images

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const express = require('express');
 const app = express();
 
 app.use(cors());
+app.use(express.static('public'))
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
