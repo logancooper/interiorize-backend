05efed0b3f8ac0551f2b68eb506fdcd71b6d33b4
Logan Cooper <logancooper@bellsouth.net>
2021-08-11 11:46:56 -0400

    Adjusted backend schema

---
+++ sql/schema.sql
@@ -0,0 +1,60 @@
+CREATE TABLE users(
+    id serial PRIMARY KEY,
+    auth0_id text,
+    first_name text,
+    last_name text,
+    email varchar(200),
+    UNIQUE(email)
+);
+
+CREATE TABLE items(
+    id serial PRIMARY KEY,
+    description text,
+    img_src text,
+    price float,
+    brand text,
+    color_id integer REFERENCES colors(id)
+);
+
+CREATE TABLE orders(
+    id serial PRIMARY KEY,
+    user_id integer REFERENCES users(id),
+    created_date TIMESTAMP
+);
+
+CREATE TABLE orders_items(
+    order_id integer REFERENCES orders(id),
+    item_id integer REFERENCES items(id)
+);
+
+CREATE TABLE categories(
+    id serial PRIMARY KEY,
+    category_name text
+);
+
+CREATE TABLE item_categories(
+    item_id integer REFERENCES items(id),
+    category_id integer REFERENCES categories(id)
+);
+
+CREATE TABLE tags(
+    id serial PRIMARY KEY,
+    tag_description text
+);
+
+CREATE TABLE items_tags(
+    item_id integer REFERENCES items(id),
+    tag_id integer REFERENCES tags(id)
+);
+
+CREATE TABLE colors(
+    id serial PRIMARY KEY,
+    color_name text
+);
+
+CREATE TABLE reviews(
+    id serial PRIMARY KEY,
+    user_id integer REFERENCES users(id),
+    item_id integer REFERENCES items(id),
+    rating integer
+);
